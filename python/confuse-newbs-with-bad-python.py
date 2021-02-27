a856f03592afa9fd00e2f7349d05c9dc5dd1449b
tooboredtocode <bored-coder@tooboredtocode.dev>
2021-02-27 14:37:55 +0100

    fix: confuse-newbs-with-bad-python.py

--- python/confuse-newbs-with-bad-python.py
+++ python/confuse-newbs-with-bad-python.py
@@ -1,1 +1,1 @@
-print("Take an umbrella") if __import__("re").match(r"^y$"gmi, input("Is it raining? ")) else print("Have a nice day")
+print("Take an umbrella") if __import__("re").match(r"^y$", input("Is it raining? "), __import__("re").IGNORECASE) else print("Have a nice day")
