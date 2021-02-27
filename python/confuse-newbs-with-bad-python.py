cd3b464ab2c5a937dc144eb5c07f284102030549
vcokltfre <vcokltfre@gmail.com>
2021-02-27 13:44:42 +0000

    fix: make code worse
    Always, **always** take an umbrella with you.

--- python/confuse-newbs-with-bad-python.py
+++ python/confuse-newbs-with-bad-python.py
@@ -1,1 +1,1 @@
-print("Take an umbrella") if __import__("re").match(r"^y$", input("Is it raining? "), __import__("re").IGNORECASE) else print("Have a nice day")
+print(("Take an umbrella.", "Have a nice day!")[bool(__import__('re').match(r"^y$", input("Is it raining? "), __import__("re").IGNORECASE))])
